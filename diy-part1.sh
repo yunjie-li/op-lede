3d49ea9d403e58824c729719c85a4510b3e90320
yunjie <yunjie-li@users.noreply.github.com>
2022-03-19 23:21:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,7 @@
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # git clone https://github.com/kenzok8/luci-theme-ifit.git package/lean/luci-theme-ifit
 
