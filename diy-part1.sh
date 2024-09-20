7f98f1d7f118e4d0f203e8ff2c60a571d46b5e74
yunjie <yunjie-li@users.noreply.github.com>
2024-09-21 07:35:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,7 @@
 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+git pull
 
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
