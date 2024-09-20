41b64c91b5f13b4e4d5c1b8c27a3d3d6e8a0d526
yunjie <yunjie-li@users.noreply.github.com>
2024-09-20 19:10:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,8 +22,6 @@
 
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
-
 # rm -rf feeds/packages/lang/golang
 # git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
