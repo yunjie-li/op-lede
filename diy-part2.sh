983725ad825765582570dda9469bdf79077140d9
yunjie <yunjie-li@users.noreply.github.com>
2024-09-22 08:06:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,9 @@
 # Modify default IP
 
 sed -i 's/192.168.1.1/192.168.2.253/g' package/base-files/files/bin/config_generate
+
+# 更改默认 Shell 为 zsh
+sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+
+# TTYD 免登录
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
